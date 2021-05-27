4ba70ac401602446ed6e2f6563ca5ce5dda6e74a
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-27 20:06:15 +0200

    Klasa Kulka i jej implementacja

--- src/pong1/Pong.java
+++ src/pong1/Pong.java
@@ -26,10 +26,7 @@ public class Pong extends Application {
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R = 10;
     private static final int LICZBAKULEK = 10;
-    private double[] x = new double[LICZBAKULEK];
-    private double[] y = new double[LICZBAKULEK];
-    private double[] vx = new double[LICZBAKULEK];
-    private double[] vy = new double[LICZBAKULEK];
+    private Kulka[] kulki = new Kulka[LICZBAKULEK];
 
     @Override
     public void start(Stage stage) {
@@ -61,18 +58,9 @@ public class Pong extends Application {
         gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
 
         for (int i = 0; i < LICZBAKULEK; i++){
-            if ((x[i] <= ARENAX1) || (x[i] >= ARENAX2)) vx[i] = -vx[i];
-            if ((y[i] <= ARENAY1) || (y[i] >= ARENAY2)) vy[i] = -vy[i];
-        }
-
-        for (int i = 0; i < LICZBAKULEK; i++){
-            x[i] += vx[i];
-            y[i] += vy[i];
-        }
-
-        for (int i = 0; i < LICZBAKULEK; i++){
-            gc.setFill(Color.WHITESMOKE);
-            gc.fillOval(x[i] - R, y[i] - R, 2*R, 2*R);
+            kulki[i].checkBoundaryCollision(ARENAX1, ARENAY1, ARENAX2, ARENAY2);
+            kulki[i].update();
+            kulki[i].draw(gc);
         }
 
     }
@@ -80,10 +68,11 @@ public class Pong extends Application {
     private void initKula(){
         Random lott = new Random();
         for (int i = 0; i < LICZBAKULEK; i++){
-            x[i] = lott.nextDouble() * ARENAWIDTH + ARENAX1;
-            y[i] = lott.nextDouble() * ARENAHEIGHT + ARENAY1;
-            vx[i] = 5+lott.nextDouble()*20;
-            vy[i] = 5+lott.nextDouble()*20;
+            kulki[i] = new Kulka(
+                    lott.nextDouble() * ARENAWIDTH + ARENAX1,
+                    lott.nextDouble() * ARENAHEIGHT + ARENAY1,
+                    5 + lott.nextDouble() * 20,
+                    5 + lott.nextDouble() * 20);
         }
 
 
